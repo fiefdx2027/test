bd4b7727e0b9e2ee07cd90db5a226bb81a03878a
fiefdx2027 <fiefdx2027@gmail.com>
2026-03-20 06:52:16 -0600

    Add hello_world.py example script

---
+++ hello_world.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+Hello World Example - A simple Python script to demonstrate basic functionality.
+This is a classic "Hello World" program used to test Python installations.
+"""
+
+def hello_world():
+    """Print a simple hello world message."""
+    print("Hello, World!")
+    print("Welcome to the Python Test Repository!")
+    print("Repository: https://github.com/fiefdx2027/test")
+    print("Created by nanobot 🐈")
+
+def main():
+    """Main function to run the hello world example."""
+    hello_world()
+    print("\n" + "=" * 50)
+    print("This is a sample Python project created by nanobot 🐈")
+    print("Feel free to modify and extend this code!")
+    print("=" * 50)
+
+if __name__ == "__main__":
+    main()
